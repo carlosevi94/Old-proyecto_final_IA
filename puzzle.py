9c8c7d3cfa2ff0e1c4c4eb4ecfc1df54eb402fdb
FerminFB <ferminffb@gmail.com>
2016-06-08 15:54:51 +0200

    Encontrar Hueco
    Añadido el metodo para encontrar el hueco del N-Puzzle y mejorados el
    resto de metodos.

--- puzzle.py
+++ puzzle.py
@@ -3,49 +3,69 @@ import numpy
 import math
 
 class puzzle:
+    matrix=""
+    estadoFinal=False
+    M=""
+    tamano=0
     
-    
-    def creaEstadoInicial(self,tamano):
-        M=math.sqrt(tamano+1)
-        matrix=numpy.zeros((M,M))
-        lista=[]
+    def __init__(self,tamano):
+        self.tamano=tamano
+        self.M = int(math.sqrt(tamano + 1))
+        self.estadoFinal = numpy.zeros((self.M, self.M))
+        lista = []
         c=0
-        for n in range (tamano+1):
+        for n in range (tamano + 1):
             lista.append(n)
-        for i in range (int (M)):
-            for j in range (int (M)):
-                r=numpy.random.randint(tamano+1-c)
-                c=c+1
-                matrix[i][j]=lista[r]
-                lista.pop(r)
-        return matrix
+        for i in range (self.M):
+            for j in range(self.M):
+                self.estadoFinal[i][j] = lista[c]
+                c = c + 1
         
-    def esSolucion(self,estado,tamano):
-        res=True
-        M=math.sqrt(tamano+1)
-        solucion=numpy.zeros((M,M))
-        c=0
-        lista=[]
-        for n in range (tamano+1):
+    def creaEstadoInicial(self):
+        M = math.sqrt(self.tamano + 1)
+        self.matrix = numpy.zeros((M, M))
+        lista = []
+        for n in range (self.tamano + 1):
             lista.append(n)
         for i in range (int (M)):
             for j in range (int (M)):
-                solucion[i][j]=lista[c]
-                c=c+1
-        print (solucion)
-        print (estado)
-        for i in range (int (M)):
-            for j in range (int (M)):
-                a=solucion[i][j]
-                b=estado[i][j]
-                if(a!=b):
-                    res=False
+                r = numpy.random.randint(len(lista))
+                self.matrix[i][j] = lista[r]
+                lista.pop(r)
+        
+    def esSolucion(self):
+        res = True
+        for i in range (self.M):
+            for j in range (self.M):
+                a = self.estadoFinal[i][j]
+                b = self.matrix[i][j]
+                if(a != b):
+                    res = False
                     break
         return res
-#Pruebas Varias
-'''for n in range (100):
-    puzzle.creaEstadoInicial(puzzle,8)'''
-s=numpy.matrix('0.,1.,2.;3.,4.,5.;6.,7.,8.')
+    
+    def encontrarHueco(self):
+        res=(-1,-1)
+        for i in range(self.M):
+            for j in range(self.M):
+                if self.matrix[i][j]==0:
+                    res=(i,j)
+        return res
+            
+    
+    def expandirNodo(self):
+        
+        return 
+ 
+ 
+'''----------------------------------------------------------'''
+   
+e=puzzle(15)
+e.creaEstadoInicial()
+print(e.matrix)
+print(e.estadoFinal)
+print(e.esSolucion())
+print(e.encontrarHueco())
 
-print(puzzle.esSolucion(puzzle,puzzle.creaEstadoInicial(puzzle, 8),8))
 
+    
