19fdd996d9c86a283f325e4263c0e26c1569faea
FerminFB <ferminffb@gmail.com>
2016-06-09 20:57:51 +0200

    Busqueda en haz
    Empezado el modulo busqueda_en_haz que implementa un algoritmo de
    busqueda en haz

--- buqueda_en_haz.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class busqueda_en_haz(estadoInicial,heuristica,entero,memoria):
-    
-    def inicio:
-        estadoInicial
-    
-    
---
+++ búsqueda_en_haz.py
@@ -0,0 +1,13 @@
+def BusquedaHaz(self,estadoInicial,memoriaMax,haz):
+    memoria=[]
+    nivelActual=1
+    memoria.append(estadoInicial)
+    sol=estadoInicial.esSolucion()
+    nodoActual=estadoActual
+    while sol==False:
+        hijos=nodoActual.expandirNodo()
+    
+    
+
+    
+    
