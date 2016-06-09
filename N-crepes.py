ee5d7f6daf87bc4a1a6e921446d43644df19c785
carlosevi94 <carlosevillabarcelo@hotmail.com>
2016-06-09 16:14:28 +0200

    Creados archivos ncrepes y busquedaenhaz

---
+++ N-crepes.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import numpy
+import math
+
+class crepes:
+    matrix=""
+    estadoFinal=False
+    M=""
+    tamano=0
+    
+    def __init__(self,tamano):
+       
+        
+    def creaEstadoInicial(self):
+     
+        
+    def esSolucion(self):
+        
+    
+    def encontrarPos(self,pos,op):
+        
+            
+    
+    def expandirNodo(self):
+        
+    
+
+    def clonaEstado(self):
+        
+      
+    def __repr__(self):
+        return repr(self.matrix)
+    
+    def getHeuristica(self):
+       
+'''----------------------------------------------------------'''
+   
+e=puzzle(8)
+e.creaEstadoInicial()
+print(e.matrix)
+print(e.estadoFinal)
+'''print(e.esSolucion())
+print(e.encontrarPos(0,True))
+hijos=e.expandirNodo()
+for n in hijos:
+    print (n)'''
+print(e.getHeuristica())
+
+
+    
---
+++ buqueda_en_haz.py
@@ -0,0 +1,9 @@
+
+
+
+class busqueda_en_haz(estadoInicial,heuristica,entero,memoria):
+    
+    def inicio:
+        estadoInicial
+    
+    
